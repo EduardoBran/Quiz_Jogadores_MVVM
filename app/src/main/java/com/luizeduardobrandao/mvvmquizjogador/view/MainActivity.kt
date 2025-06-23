3dbfbb21aec0c187b001df5cc1257cf2815d82a3
EduardoBran <eduardo.ads1814@gmail.com>
2025-06-23 13:11:50 -0300

    adicionando evento de clique para navegar de MainActivity para LevelsActivity

--- app/src/main/java/com/luizeduardobrandao/mvvmquizjogador/view/MainActivity.kt
+++ app/src/main/java/com/luizeduardobrandao/mvvmquizjogador/view/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.luizeduardobrandao.mvvmquizjogador.view
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.luizeduardobrandao.mvvmquizjogador.R
 import com.luizeduardobrandao.mvvmquizjogador.databinding.ActivityMainBinding
+import com.luizeduardobrandao.mvvmquizjogador.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,5 +31,26 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        setListeners()
+        setObservers()
     }
+
+    // Disparar evento no ViewModel
+    private fun setListeners(){
+        binding.btnStart.setOnClickListener {
+            viewModel.onStartQuizClicked()
+        }
+    }
+
+    // Observar o evento e navegar
+    private fun setObservers() {
+        viewModel.navigateToLevels.observe(this) { shouldNavigate ->
+            if (shouldNavigate){
+                startActivity(Intent(this, LevelsActivity::class.java))
+                viewModel.onNavigationDone()
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/luizeduardobrandao/mvvmquizjogador/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/luizeduardobrandao/mvvmquizjogador/viewmodel/MainViewModel.kt
@@ -1,4 +1,23 @@
 package com.luizeduardobrandao.mvvmquizjogador.viewmodel
 
-class MainViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
+class MainViewModel(application: Application): AndroidViewModel(application) {
+
+    // Evento de navegação para LevelsActivity
+    private val _navigateToLevels = MutableLiveData<Boolean>()
+    val navigateToLevels: LiveData<Boolean> = _navigateToLevels
+
+    // Chamar ao clicar no botão “Iniciar”
+    fun onStartQuizClicked(){
+        _navigateToLevels.value = true
+    }
+
+    // Após navegar, resetar o evento
+    fun onNavigationDone(){
+        _navigateToLevels.value = false
+    }
 }
