7d43049478515af936b4d2b37ef468fc88590a34
EduardoBran <eduardo.ads1814@gmail.com>
2025-06-23 12:27:43 -0300

    adicionando layout da main activity ja configurado com dimens

--- app/src/main/java/com/luizeduardobrandao/mvvmquizjogador/view/MainActivity.kt
+++ app/src/main/java/com/luizeduardobrandao/mvvmquizjogador/view/MainActivity.kt
@@ -19,6 +19,9 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // 2) Configura a Toolbar (binding.toolbarMain) como ActionBar da Activity
+        setSupportActionBar(binding.toolbarMain)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
