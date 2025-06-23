9c8946f701e1c5526e318c92f02950410ef6b128
EduardoBran <eduardo.ads1814@gmail.com>
2025-06-23 14:14:57 -0300

    atualizando layout de levels com recycler view e criando item_level e level_button_color para gerenciar exibicao e mudanca de cor dos botoes

--- app/src/main/java/com/luizeduardobrandao/mvvmquizjogador/view/LevelsActivity.kt
+++ app/src/main/java/com/luizeduardobrandao/mvvmquizjogador/view/LevelsActivity.kt
@@ -7,6 +7,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.luizeduardobrandao.mvvmquizjogador.R
 import com.luizeduardobrandao.mvvmquizjogador.databinding.ActivityLevelsBinding
+import com.luizeduardobrandao.mvvmquizjogador.repository.BannerAds
 
 class LevelsActivity : AppCompatActivity() {
 
@@ -19,10 +20,16 @@ class LevelsActivity : AppCompatActivity() {
         binding = ActivityLevelsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Configura a Toolbar (binding.toolbarMain) como ActionBar da Activity
+        setSupportActionBar(binding.toolbarLevels)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // carrega o banner no container da view binding
+        BannerAds.loadBanner(this, binding.frameBanner)
     }
 }
